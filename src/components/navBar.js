8102111ea62442a61f63f7e4d7dfb488f571475b
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-05 20:16:31 -0300

    Bootstrap small changes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -9,7 +9,7 @@ export const NavBar = () => {
 					<p id="graph">Encontrá todo lo necesario para crecer en el ecosistema crypto</p>
 				</div> 
 				<div className="col-lg-8 offset-lg-2 offset-3 offset-sm-4 d-flex justify-content-lg-around"> 
-					<nav id="mainNav" class="navbar navbar-expand-lg navbar-light bg-light dropdown">
+					<nav id="mainNav" className="navbar navbar-expand-lg navbar-light bg-light dropdown">
 						<a id="home"class="navbar-brand" href="#">Home</a>
 						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
@@ -21,14 +21,14 @@ export const NavBar = () => {
 								<li className="nav-item active">
 										<a id="keyCards" class="nav-link" href="#">Key Cards</a>
 								</li>
-								<li class="nav-item active dropdown">
-									<a class="nav-link dropdown-toggle" data-toggle="dropdown"
+								<li className="nav-item active dropdown">
+									<a className="nav-link dropdown-toggle" data-toggle="dropdown"
 										href="#" role="button" aria-haspopup="true" aria-expanded="false">
 										Merchandising</a>
-									<div class="dropdown-menu">
-										<a id="indumentaria" class="dropdown-item" href="#!">Indumentaria</a>
-										<a id="medias" class="dropdown-item" href="#!">Medias</a>
-										<a id="accesorios" class="dropdown-item" href="#!">Accesorios</a>
+									<div className="dropdown-menu">
+										<a id="indumentaria" className="dropdown-item" href="#!">Indumentaria</a>
+										<a id="medias" className="dropdown-item" href="#!">Medias</a>
+										<a id="accesorios" className="dropdown-item" href="#!">Accesorios</a>
 									</div>
 								</li>
 							</ul>
