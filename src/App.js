b374e6b15d53e2eed14ecf33d87630141cb202fd
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-05 19:36:39 -0300

    Creación del NavBar e importación de Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/navBar';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "jquery/dist/jquery.min.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Gretto's
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/navBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export const NavBar = () => {
+	return (
+		<div>
+			<div className="row">
+				<div id="titleGraph" class="col-lg-8 offset-lg-2">
+					<h1>Grettos</h1>
+					<p id="graph">Encontrá todo lo necesario para crecer en el ecosistema crypto</p>
+				</div> 
+				<div className="col-lg-8 offset-lg-2 offset-3 offset-sm-4 d-flex justify-content-lg-around"> 
+					<nav id="mainNav" class="navbar navbar-expand-lg navbar-light bg-light">
+						<a id="home"class="navbar-brand" href="#">Home</a>
+						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+						</button>
+						<div className="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul className="navbar-nav mr-auto">
+								<li className="nav-item active">
+										<a id="hardwareWallets" class="nav-link" href="#">Hardaware Wallets</a>
+								</li>
+								<li className="nav-item active">
+										<a id="keyCards" class="nav-link" href="#">Key Cards</a>
+								</li>
+								<li class="nav-item dropdown active">
+									<a class="nav-link dropdown-toggle" data-toggle="dropdown"
+										href="#" role="button" aria-haspopup="true" aria-expanded="false">
+										Merchandising</a>
+									<div class="dropdown-menu">
+										<a id="indumentaria" class="dropdown-item" href="#!">Indumentaria</a>
+										<a id="medias" class="dropdown-item" href="#!">Medias</a>
+										<a id="accesorios" class="dropdown-item" href="#!">Accesorios</a>
+									</div>
+								</li>
+							</ul>          
+						</div>
+					</nav>
+				</div>  
+			</div>
+		</div>	
+	)
+}
