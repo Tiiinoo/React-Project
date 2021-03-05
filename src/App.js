a9ccb36e707640bd973cf2b90595b95a74b91628
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-05 20:03:08 -0300

    Bootstrap small changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { NavBar } from './components/navBar';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "jquery/dist/jquery.min.js"
 
+
 function App() {
   return (
     <div className="App">
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -9,12 +9,11 @@ export const NavBar = () => {
 					<p id="graph">Encontrá todo lo necesario para crecer en el ecosistema crypto</p>
 				</div> 
 				<div className="col-lg-8 offset-lg-2 offset-3 offset-sm-4 d-flex justify-content-lg-around"> 
-					<nav id="mainNav" class="navbar navbar-expand-lg navbar-light bg-light">
+					<nav id="mainNav" class="navbar navbar-expand-lg navbar-light bg-light dropdown">
 						<a id="home"class="navbar-brand" href="#">Home</a>
 						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 						</button>
-						<div className="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul className="navbar-nav mr-auto">
 								<li className="nav-item active">
 										<a id="hardwareWallets" class="nav-link" href="#">Hardaware Wallets</a>
@@ -22,7 +21,7 @@ export const NavBar = () => {
 								<li className="nav-item active">
 										<a id="keyCards" class="nav-link" href="#">Key Cards</a>
 								</li>
-								<li class="nav-item dropdown active">
+								<li class="nav-item active dropdown">
 									<a class="nav-link dropdown-toggle" data-toggle="dropdown"
 										href="#" role="button" aria-haspopup="true" aria-expanded="false">
 										Merchandising</a>
@@ -32,8 +31,7 @@ export const NavBar = () => {
 										<a id="accesorios" class="dropdown-item" href="#!">Accesorios</a>
 									</div>
 								</li>
-							</ul>          
-						</div>
+							</ul>
 					</nav>
 				</div>  
 			</div>
