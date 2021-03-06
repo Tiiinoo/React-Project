8282b82523804e1d608824901b6223cc44a03334
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-06 16:34:59 -0300

    NavBar styles added

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,25 +1,26 @@
 import React from 'react'
+import './navBar.css'
 
 export const NavBar = () => {
 	return (
 		<div>
 			<div className="row">
-				<div id="titleGraph" class="col-lg-8 offset-lg-2">
+				<div id="titleGraph" className="col-lg-8 offset-lg-2">
 					<h1>Grettos</h1>
 					<p id="graph">Encontrá todo lo necesario para crecer en el ecosistema crypto</p>
 				</div> 
 				<div className="col-lg-8 offset-lg-2 offset-3 offset-sm-4 d-flex justify-content-lg-around"> 
 					<nav id="mainNav" className="navbar navbar-expand-lg navbar-light bg-light dropdown">
-						<a id="home"class="navbar-brand" href="#">Home</a>
+						<a id="home"className="navbar-brand" href="#">Home</a>
 						<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 						</button>
 							<ul className="navbar-nav mr-auto">
 								<li className="nav-item active">
-										<a id="hardwareWallets" class="nav-link" href="#">Hardaware Wallets</a>
+										<a id="hardwareWallets" className="nav-link" href="#">Hardaware Wallets</a>
 								</li>
 								<li className="nav-item active">
-										<a id="keyCards" class="nav-link" href="#">Key Cards</a>
+										<a id="keyCards" className="nav-link" href="#">Key Cards</a>
 								</li>
 								<li className="nav-item active dropdown">
 									<a className="nav-link dropdown-toggle" data-toggle="dropdown"
