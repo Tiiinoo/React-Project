95225c89fd0b705502cb82cc060c291dbe103e3a
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-06 16:56:43 -0300

    Dropdown menu up and classes corrected

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { NavBar } from './components/navBar';
-import "bootstrap/dist/css/bootstrap.min.css"
-import "jquery/dist/jquery.min.js"
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+
 
 
 function App() {
