cdf5db61f939b197231262ba927a6053401df5fe
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-10 09:59:18 -0300

    Prop greeting small change

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -5,7 +5,7 @@ export const ItemListContainer = (prop) => {
 	return (
 		<div className="row">
 			<div className="col-lg-8 offset-lg-2">
-				<h3 id="itemsTitle">Hola {prop.greeting} chequeá nuestra lista de productos</h3>
+				<h3 id="itemsTitle">Hola {prop.greeting}! Chequeá nuestra lista de productos</h3>
 			</div>
 		</div>	
 	)	
