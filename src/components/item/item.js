a1f3a670dde693b787145d9b0391ece8b5ea803a
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-22 20:17:45 -0300

    Img alt corrected

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -4,7 +4,7 @@ import './item.css'
 export const Item = ({ items }) => {
 	return (
 		<div className="card">
-			<img className="card-img-top" src={items.image} alt="Card image cap" />
+			<img className="card-img-top" src={items.image} alt={items.name + " image"}  />
 			<div className="card-body">
 				<h4 className="card-title">{items.name}</h4>
 				<h5 className="card-title">${items.price}</h5>
