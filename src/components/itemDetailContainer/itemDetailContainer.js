c74370e57921e1dcca71718b227e1a941b5702d3
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-25 23:02:29 -0300

    7th class challenge completed

--- src/components/itemDetailContainer/itemDetailContainer.js
+++ src/components/itemDetailContainer/itemDetailContainer.js
@@ -2,32 +2,30 @@ import React, { useState, useEffect } from 'react'
 import './itemDetailContainer.css'
 import ItemDetail  from '../itemDetail/itemDetail'
 
-const getItem = () => {
-	return new Promise((resolve)=>{
-	setTimeout(() => {resolve({
-		image: 'https://www.diariobitcoin.com/wp-content/uploads/2021/02/calcetines-unisocks-web.jpg',
-		name : 'Uniswap socks',
-		price: '10',
-		description: 'Get your feet warm and full of liquidity'
-		})}, 2000)
-})}
-
 export default function ItemDetailContainer() {
 	const [item, setItem] = useState(null)
-	useEffect(() => {
-		getItem().then((res) =>
-			setItem(res))
-			return;
-	}, [])
-	
-	return (
-				<div className="container">	
+	const [loaded, setloaded] = useState(false);
+	const promesa = new Promise((resolve)=>{
+		setTimeout(() => {resolve({
+			image: 'https://www.diariobitcoin.com/wp-content/uploads/2021/02/calcetines-unisocks-web.jpg',
+			name : 'Uniswap socks',
+			price: '10',
+			description: 'Get your feet warm and full of liquidity'
+			})}, 2000)
+	})
+	promesa.then(res => {
+		setItem(res)
+		setloaded(true);
+	});
+	return (	<div>
+				{loaded && <div className="container">	
 					<div className="row justify-content-center">
 					<h3>Indumentary section</h3>
 					</div>
 					<div className="row justify-content-center">
 						<ItemDetail item={ item } />
 					</div>	
-				</div>	
+				</div>	}
+	</div>
 	)
 }
