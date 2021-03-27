2c7f1e2b358ec11fd7013625f2543629d774d856
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-03-27 19:08:09 -0300

    React-route-dom up

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import "./icomoon/style.css";
 import { ItemListContainer } from './components/itemListContainer/itemListContainer';
 import { ItemCount } from './components/itemCount/itemCount';
 import ItemDetailContainer from './components/itemDetailContainer/itemDetailContainer';
-
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
-			<main className="App-main">
-				<ItemListContainer greeting="cyptoUser" />
-				<ItemCount initial="0" quantity="5" />
-				<ItemDetailContainer />
-			</main>
-    </div>
+    <BrowserRouter>
+			<div className="App">
+				<header className="App-header">
+					<NavBar />
+				</header>
+				<main className="App-main">
+					<Switch>
+						<Route exact path='/'>
+							<ItemListContainer greeting="cyptoUser" />
+						</Route>
+						<Route path="/count">
+							<ItemCount initial="0" quantity="5" />
+						</Route>
+						<Route path='*'>
+							ERROR 404!
+						</Route>
+					</Switch>
+				</main>
+			</div>
+		</BrowserRouter>
   );
 }
 
