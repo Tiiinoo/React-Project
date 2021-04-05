4cad9027d1d902babfed5d4c346b74c08edd3312
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-04-05 19:10:20 -0300

    Categories filter up and working

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './itemList.css'
 import { Item } from '../item/item'
+import {useParams} from "react-router-dom"
 
 export const ItemList = ({items=[]}) => {
+	const {id} = useParams()
+	
 	return (
 		<div className="card-columns col-lg-8 offset-lg-2">
-			{items.map(items => <Item key={items.id} items={items} /> )}
+			{items.map(items => <Item key={items.id} items={ items } /> )}
 		</div>
 	)
 }
