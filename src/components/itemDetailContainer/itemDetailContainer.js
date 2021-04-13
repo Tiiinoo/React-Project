476459f3c53b45c91c20c82ca5becef594868c32
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-04-13 18:54:00 -0300

    Trying getting the challenge done

--- src/components/itemDetailContainer/itemDetailContainer.js
+++ src/components/itemDetailContainer/itemDetailContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './itemDetailContainer.css'
 import ItemDetail  from '../itemDetail/itemDetail'
 import { useParams } from "react-router-dom";
@@ -7,7 +7,7 @@ export default function ItemDetailContainer() {
 	const [item, setItem] = useState(null)
 	const [loaded, setloaded] = useState(false);
 	const {id} = useParams()
-	const promesa = new Promise((resolve)=>{
+	useEffect (() => { const promesa = new Promise((resolve)=>{
 		setTimeout(() => {resolve({
 			name : ''
 			})}, 2000)
@@ -15,16 +15,20 @@ export default function ItemDetailContainer() {
 	promesa.then(res => {
 		setItem(res)
 		setloaded(true);
-	},);
+		}, 
+	)}, 
+[])
+	
 	return (	<div>
-				{loaded && <div className="container">	
-					<div className="row justify-content-center">
-					<h3>{id}</h3>
-					</div>
-					<div className="row justify-content-center">
-						<ItemDetail item={ item } />
-					</div>	
-				</div>	}
-	</div>
+							{loaded && 
+							<div className="container">	
+								<div className="row justify-content-center">
+									<h3>{id}</h3>
+								</div>
+								<div className="row justify-content-center">
+									<ItemDetail item={ item } />
+								</div>	
+							</div>}
+						</div>
 	)
 }
